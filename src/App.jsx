615fc16a6fb1932a45cef0ec60f4935bb5a0715f
sara <sara2003me@gmail.com>
2025-02-06 19:00:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <>
+      <div className='bg-primry_purble'>
+        hii
+        <div className="bg-veryLight_purple">
+          sara
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+
+    extend: {
+      colors: {
+        primry_purble: "#7F00FF",
+        veryLight_purple:"#F2E5FF",
+        
+      }
+      
+    },
+  },
+plugins: [],
+}
+
