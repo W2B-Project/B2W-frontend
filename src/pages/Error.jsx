87d97b0aca147cd0af1e3d606805bdbe3a69b9b6
Mohamed Khaled <mohamedkhaledtharwar123@gmail.com>
2025-02-25 00:16:38 +0200

    404 page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import NotFoundImage from "../assets/images/404.png";
+import Button from "../components/global/Button";
+
+function Error() {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center text-center">
+            <img src={NotFoundImage} alt="Not Found" className="" />
+            <h1 className="text-5xl md:text-6xl leading-10 font-bold">
+                Oops! <span className="text-primry_purble">Page not Found</span>
+            </h1>
+            <p className="text-gray-600  max-w-md text-xl md:text-2xl leading-6 md:leading-8 font-normal mt-7">
+                The page you are looking for cannot be found. Take a break before trying again.
+            </p>
+            <Link to="/">
+                <Button btn_text="Go To Home Page" />
+            </Link>
+
+        </div>
+    );
+}
+
+export default Error;
