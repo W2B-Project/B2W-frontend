923518e7573b06fee5bb2a67ec3c488a6f305f63
Mohamed Khaled <mohamedkhaledtharwat123@gmail.com>
2025-03-18 00:51:02 +0200

    f

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,9 +6,9 @@ import PeopleList from "../../components/home/PeopleList";
 
 function Home() {
     return (
-        <div className="px-8 h-auto bg-[#f4f4f6]">
+        <div className=" h-auto bg-[#f4f4f6]">
             <Header />
-            <div className="flex justify-between gap-8">
+            <div className="px-8 flex justify-between gap-8">
                 <ProfileMenu />
                 <div className="flex flex-col gap-3 w-full">
                     <Outlet />
