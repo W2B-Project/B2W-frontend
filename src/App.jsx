fffcb97c5a8c99bf09584cfa52724732d2ce770f
a7mad7asann <ahmadembaby333@gmile.com>
2025-05-04 00:44:21 +0300

    company home page : add twe aside sections

--- src/App.jsx
+++ src/App.jsx
@@ -87,18 +87,23 @@ function App() {
               {/* userProfile Routes */}
               <Route path='/userProfile' element={<UserProfile />}>
                 <Route index element={<Posts />} />
+                
                 <Route path="posts" element={<Posts />} />
+                
                 <Route path="Info" element={<ProfessionalInfo />} >
                   <Route path="edit" element={<EditAllPage />} />
                   <Route path="editSkills" element={<EditSkills />} />
                   <Route path="editCV" element={<EditResume />} />
                 </Route>
+                
                 <Route path="Achievements" element={<Achievements />}>
                   <Route path="edit" element={<EditAllPage />} />
                 </Route>
+                
                 <Route path="Saved" element={<Saved />} />
               </Route>
               {/* Company Home */}
+                
                 <Route path="/home-Company" element={<HomeCom />}>
                   <Route index element={<Applications />} />
                   <Route path="applications" element={<Applications />} />
@@ -107,6 +112,7 @@ function App() {
                 </Route>
                 <Route path="EditAccessability" element={<EditAccessability />} />
               {/* company Profile Routes */}
+              
               <Route path='/companyProfile' element={<CompanyProfile />}>
                 <Route index element={<About />} />
                 <Route path="about" element={<About />} />
@@ -116,8 +122,10 @@ function App() {
                 </Route>
                 <Route path="reviews" element={<Reviews />} />
               </Route>
+              
               <Route path="EditCompanyInfo" element={<EditCompanyInfo />} />
               {/* settigs */}
+              
               <Route path="/settings" element={<Settings />}>
                 <Route index element={<Language />} />
                 <Route path="lang" element={<Language />} />
@@ -126,6 +134,7 @@ function App() {
                 <Route path="logout" element={<Logout />} />
               </Route>
               {/* error routes */}
+              
               <Route path="*" element={<Error />} />
             </Routes>
           </JobProvider>
--- src/Company/pages/HomeCom.jsx
+++ src/Company/pages/HomeCom.jsx
@@ -1,9 +1,34 @@
 import Header from '../../components/home/Header'
 
+//those are the two components that are used in the home page aside left and right 
+import Aside_profile from '../components/CompanyProfile/Company_Home/Aside_profile'
+import Aside_people from '../components/CompanyProfile/Company_Home/Aside_people'
+
 function HomeCom() {
   return (
     <>
-    <Header usertype={'company'}/>
+      <Header usertype={'company'}/>
+      
+      <div className='bg-light_gray'>
+        <div className="body  pt-28 flex  items-start justify-center gap-6 w-full">
+    
+          
+            <Aside_profile />
+        
+
+          
+
+          <div className="content w-[684px]">
+            <h1>Company Home</h1>
+            <p>Welcome to the company home page!</p>
+          </div>
+
+          
+          <Aside_people />
+
+          
+          </div>
+      </div>
     </>
   )
 }
